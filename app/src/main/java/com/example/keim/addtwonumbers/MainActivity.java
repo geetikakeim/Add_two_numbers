e74977c29609d1a4727199a878604b5ad03c7dc0
Geetika keim <geetikakeim04@gmail.com>
2019-10-12 22:13:43 +0530

    test committed

--- app/src/main/java/com/example/keim/addtwonumbers/MainActivity.java
+++ app/src/main/java/com/example/keim/addtwonumbers/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.keim.addtwonumbers;
 
+ import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    // define the global variable
+
+    // variable number1, number2 for input input number
+    // Add_button, result textView
+
+    EditText number1;
+    EditText number2;
+    Button Add_button;
+    TextView result;
+    int ans=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // by ID we can use each component which id is assign in xml file
+        number1=(EditText) findViewById(R.id.editText_first_no);
+        number2=(EditText) findViewById(R.id.editText_second_no);
+        Add_button=(Button) findViewById(R.id.add_button);
+        result = (TextView) findViewById(R.id.textView_answer);
+
+        // Add_button add clicklistener
+        Add_button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                // num1 or num2 double type
+                // get data which is in edittext, convert it to string
+                // using parse Double convert it to Double type
+                double num1 = Double.parseDouble(number1.getText().toString());
+                double num2 = Double.parseDouble(number2.getText().toString());
+                // add both number and store it to sum
+                double sum = num1 + num2;
+                // set it ot result textview
+                result.setText(Double.toString(sum));
+            }
+        });
     }
 }
